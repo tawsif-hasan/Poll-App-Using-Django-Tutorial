d28a92130a3b3ec7cc3c7d686299297dba614ebb
Mohammad Tawsif Hasan <tawsifhasan@iut-dhaka.edu>
2021-01-24 19:43:15 +0600

    Three Views Added and routed

---
+++ poll/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("Hello. your are at the polls Index.")
+
+def detail(request,question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request,question_id):
+    return HttpResponse("You're looking at results of question %s" % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You're voting at question %s" % question_id)
+
+
+
+
