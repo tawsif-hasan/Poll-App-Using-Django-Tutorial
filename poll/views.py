8df92c5c45d3b70a088b5c6219cbd254baad5027
Mohammad Tawsif Hasan <tawsifhasan@iut-dhaka.edu>
2021-01-24 21:11:40 +0600

    New Template for Index Page Created

--- poll/views.py
+++ poll/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from poll.models import Question,Choice
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Hello. your are at the polls Index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ','.join(q.question_text for q in latest_question_list)
+    return HttpResponse(output)
 
 def detail(request,question_id):
     return HttpResponse("You're looking at question %s." % question_id)
